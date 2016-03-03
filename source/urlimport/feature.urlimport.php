fcced27088d241d02df89280ce52744e4d2de5f2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-03-03 19:57:54 +0200

    # [LOW] The Import from URL feature couldn't find the Joomla! download URL after the changes made to joomla.org

--- source/urlimport/feature.urlimport.php
+++ source/urlimport/feature.urlimport.php
@@ -561,31 +561,25 @@ function AKURLerrorHandler(msg)
 			return '';
 		}
 
-		$uniqueHTMLid = '<a id="latest"';
+		$pos_start = stripos($pageHTML, 'class="download"');
 
-		$posStart = strpos($pageHTML, $uniqueHTMLid);
-
-		if ($posStart === false)
+		if ($pos_start === false)
 		{
 			return '';
 		}
 
-		$posStart += strlen($uniqueHTMLid);
-		$posEnd = strpos($pageHTML, '>', $posStart);
+		$pos_end = stripos($pageHTML, '>', $pos_start);
 
-		if ($posEnd === false)
+		if ($pos_end === false)
 		{
 			return '';
 		}
 
-		$innerAttributes = substr($pageHTML, $posStart, $posEnd - $posStart);
+		$innerContent = substr($pageHTML, $pos_start, $pos_end - $pos_end - 1);
+		$pos_start = stripos($innerContent, 'href="');
+		$pos_end = stripos($innerContent, '"', $pos_start + 6);
 
-		$posStart = strpos($innerAttributes, '"');
-		$posEnd = strpos($innerAttributes, '"', $posStart + 1);
-
-		$url = substr($innerAttributes, $posStart + 1, $posEnd - $posStart - 1);
-
-		return $url;
+		return substr($innerContent, $pos_start + 6, $pos_end - $pos_start - 6);
 	}
 }
 
