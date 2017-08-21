d6846a7f40ab9a0542bb2c4b45bc0d8689a93b45
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-08-21 15:30:58 +0300

    Move around the generated sources root

---
+++ output/readme.txt
@@ -0,0 +1,3 @@
+** Do not delete this file **
+
+This folder contains the "compiled" versions of Akeeba Restore and Akeeba Kickstart.
--- source/buildscripts/joomlastart_build.php
+++ source/buildscripts/joomlastart_build.php
@@ -12,4 +12,4 @@ include_once __DIR__ . '/../minibuild.php';
 $minibuild = new AkeebaMinibuild;
 
 $buffer = $minibuild->minibuild(MINIBUILD . '/../joomlastart.build', true);
-file_put_contents(__DIR__ . '/../output/joomlastart.php', $buffer);
+file_put_contents(__DIR__ . '/../../output/joomlastart.php', $buffer);
