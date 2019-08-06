421092c49e7c792862145a3e25786a6d2984fbdc
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-08-06 08:31:22 +0300

    # [HIGH] Language parsing causes fatal error PHP 7.4

--- source/restore/text.php
+++ source/restore/text.php
@@ -218,113 +218,124 @@ class AKText extends AKAbstractObject
 	 * Thanks to asohn ~at~ aircanopy ~dot~ net for posting this handy function on
 	 * the parse_ini_file page on http://gr.php.net/parse_ini_file
 	 *
-	 * @param string $file             Filename to process
-	 * @param bool   $process_sections True to also process INI sections
+	 * @param    string $file             Filename to process
+	 * @param    bool   $process_sections True to also process INI sections
+	 * @param    bool   $rawdata          If true, the $file contains raw INI data, not a filename
 	 *
 	 * @return array An associative array of sections, keys and values
 	 * @access private
 	 */
-	public static function parse_ini_file($file, $process_sections = false, $raw_data = false)
+	public static function parse_ini_file($file, $process_sections = false, $rawdata = false)
 	{
 		$process_sections = ($process_sections !== true) ? false : true;
 
-		if (!$raw_data)
+		if (!$rawdata)
 		{
-			$ini = @file($file);
+			$ini = file($file);
 		}
 		else
 		{
-			$ini = $file;
+			$file = str_replace("\r", "", $file);
+			$ini = explode("\n", $file);
 		}
+
+		if (!is_array($ini))
+		{
+			return array();
+		}
+
 		if (count($ini) == 0)
 		{
 			return array();
 		}
 
 		$sections = array();
-		$values   = array();
-		$result   = array();
-		$globals  = array();
-		$i        = 0;
-		if (!empty($ini))
+		$values = array();
+		$result = array();
+		$globals = array();
+		$i = 0;
+		foreach ($ini as $line)
 		{
-			foreach ($ini as $line)
+			$line = trim($line);
+			$line = str_replace("\t", " ", $line);
+
+			// Comments
+			if (!preg_match('/^[a-zA-Z0-9[]/', $line))
 			{
-				$line = trim($line);
-				$line = str_replace("\t", " ", $line);
+				continue;
+			}
 
-				// Comments
-				if (!preg_match('/^[a-zA-Z0-9[]/', $line))
-				{
-					continue;
-				}
+			// Sections
+			if ($line[0] == '[')
+			{
+				$tmp = explode(']', $line);
+				$sections[] = trim(substr($tmp[0], 1));
+				$i++;
+				continue;
+			}
 
-				// Sections
-				if ($line{0} == '[')
-				{
-					$tmp        = explode(']', $line);
-					$sections[] = trim(substr($tmp[0], 1));
-					$i++;
-					continue;
-				}
+			// Key-value pair
+			$lineParts = explode('=', $line, 2);
+			if (count($lineParts) != 2)
+			{
+				continue;
+			}
+			$key = trim($lineParts[0]);
+			$value = trim($lineParts[1]);
+			unset($lineParts);
 
-				// Key-value pair
-				list($key, $value) = explode('=', $line, 2);
-				$key   = trim($key);
-				$value = trim($value);
-				if (strstr($value, ";"))
+			if (strstr($value, ";"))
+			{
+				$tmp = explode(';', $value);
+				if (count($tmp) == 2)
 				{
-					$tmp = explode(';', $value);
-					if (count($tmp) == 2)
+					if ((($value[0] != '"') && ($value[0] != "'")) ||
+						preg_match('/^".*"\s*;/', $value) || preg_match('/^".*;[^"]*$/', $value) ||
+						preg_match("/^'.*'\s*;/", $value) || preg_match("/^'.*;[^']*$/", $value)
+					)
 					{
-						if ((($value{0} != '"') && ($value{0} != "'")) ||
-							preg_match('/^".*"\s*;/', $value) || preg_match('/^".*;[^"]*$/', $value) ||
-							preg_match("/^'.*'\s*;/", $value) || preg_match("/^'.*;[^']*$/", $value)
-						)
-						{
-							$value = $tmp[0];
-						}
-					}
-					else
-					{
-						if ($value{0} == '"')
-						{
-							$value = preg_replace('/^"(.*)".*/', '$1', $value);
-						}
-						elseif ($value{0} == "'")
-						{
-							$value = preg_replace("/^'(.*)'.*/", '$1', $value);
-						}
-						else
-						{
-							$value = $tmp[0];
-						}
-					}
-				}
-				$value = trim($value);
-				$value = trim($value, "'\"");
-
-				if ($i == 0)
-				{
-					if (substr($line, -1, 2) == '[]')
-					{
-						$globals[$key][] = $value;
-					}
-					else
-					{
-						$globals[$key] = $value;
+						$value = $tmp[0];
 					}
 				}
 				else
 				{
-					if (substr($line, -1, 2) == '[]')
+					if ($value[0] == '"')
 					{
-						$values[$i - 1][$key][] = $value;
+						$value = preg_replace('/^"(.*)".*/', '$1', $value);
+					}
+					elseif ($value[0] == "'")
+					{
+						$value = preg_replace("/^'(.*)'.*/", '$1', $value);
 					}
 					else
 					{
-						$values[$i - 1][$key] = $value;
+						$value = $tmp[0];
 					}
+				}
+			}
+			$value = trim($value);
+			$value = trim($value, "'\"");
+
+			if ($i == 0)
+			{
+				if (substr($line, -1, 2) == '[]')
+				{
+					$globals[$key][] = $value;
+				}
+				else
+				{
+					$globals[$key] = $value;
+				}
+			}
+			else
+			{
+				if (substr($line, -1, 2) == '[]')
+				{
+					$values[$i - 1][$key][] = $value;
+				}
+				else
+				{
+					$values[$i - 1][$key] = $value;
 				}
 			}
 		}
@@ -333,11 +344,17 @@ class AKText extends AKAbstractObject
 		{
 			if ($process_sections === true)
 			{
-				$result[$sections[$j]] = $values[$j];
+				if (isset($sections[$j]) && isset($values[$j]))
+				{
+					$result[$sections[$j]] = $values[$j];
+				}
 			}
 			else
 			{
-				$result[] = $values[$j];
+				if (isset($values[$j]))
+				{
+					$result[] = $values[$j];
+				}
 			}
 		}
 
